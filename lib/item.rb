01a0fc44ad169816198402ead89a6f9066ab94d1
Rion Hutsell <st.gish@gmail.com>
2020-06-10 09:29:59 -0600

    Add attr_readers name and price to item

--- lib/item.rb
+++ lib/item.rb
@@ -1,8 +1,11 @@
 class Item
+  attr_reader :name,
+              :price
 
   def initialize(item_params)
     @name = item_params[:name]
-    @price = item_params[:price]
-    
+
+    @price = item_params[:price].gsub!(/[$]/,'').to_f
+
   end
 end
