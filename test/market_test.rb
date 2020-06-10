d2fa8c8e277fc756e03728cc20faa666c0737357
Rion Hutsell <st.gish@gmail.com>
2020-06-10 10:10:14 -0600

    Add test_it_has_attributes and test_it_can_add_vendor to VendorTest

--- test/market_test.rb
+++ test/market_test.rb
@@ -8,58 +8,42 @@ class MarketTest < Minitest::Test
 
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
   end
 
   def test_it_exists
     assert_instance_of Market, @market
   end
+
+  def test_it_has_attributes
+
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
+
+  def test_it_can_add_vendor
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
+  end
 end
 
-#
-# market.name
-# #=> "South Pearl Street Farmers Market"
-#
-# market.vendors
-# #=> []
-#
-# vendor1 = Vendor.new("Rocky Mountain Fresh")
-# #=> #<Vendor:0x00007fe1348a1160...>
-#
-# item1 = Item.new({name: 'Peach', price: "$0.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-# item2 = Item.new({name: 'Tomato', price: "$0.50"})
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-# item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# #=> #<Item:0x007f9c562a5f18...>
-#
-# item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-# #=> #<Item:0x007f9c56343038...>
-#
-# vendor1.stock(item1, 35)
-#
-# vendor1.stock(item2, 7)
-#
-# vendor2 = Vendor.new("Ba-Nom-a-Nom")
-# #=> #<Vendor:0x00007fe1349bed40...>
-#
-# vendor2.stock(item4, 50)
-#
-# vendor2.stock(item3, 25)
-#
-# vendor3 = Vendor.new("Palisade Peach Shack")
-# #=> #<Vendor:0x00007fe134910650...>
-#
-# vendor3.stock(item1, 65)
-#
-# market.add_vendor(vendor1)
-#
-# market.add_vendor(vendor2)
-#
-# market.add_vendor(vendor3)
-#
-# market.vendors
+
+
 # #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe1349bed40...>, #<Vendor:0x00007fe134910650...>]
 #
 # market.vendor_names
