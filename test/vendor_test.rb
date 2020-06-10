c620cc62b25444805293dce55cf0af62838952ad
Rion Hutsell <st.gish@gmail.com>
2020-06-10 09:44:40 -0600

    Add tests for Vendor class

---
+++ test/vendor_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+
+class ItemTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_attributes
+    skip
+    expected = {}
+
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal expected, @vendor.inventory
+  end
+
+  def test_it_can_check_stock
+    skip
+    assert_equal 0, @vendor.check_stock(@item1)
+  end
+
+  def test_it_can_stock
+    skip
+    @vendor.stock(@item1, 30)
+
+    expected = {@item1 => 30}
+    assert_equal expected, @vendor.inventory
+
+    assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+    assert_equal 55, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item2, 12)
+    expected2 = {@item1 => 55, @item2 => 12}
+
+    assert_equal expected2, @vendor.inventory
+  end
+end
